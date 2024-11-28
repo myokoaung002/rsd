0396df27f8bdd650318b2f5523118d736838375d
myokoaung002 <myokoaung002@gmail.com>
2024-11-28 15:22:59 +0630

    commit

--- src/ThemedApp.jsx
+++ src/ThemedApp.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 export const AppContext = createContext();
 
 export default function ThemedApp() {
-    const [mode, setMode] = useState("dark");
+    const [mode, setMode] = useState("light");
 
     return (
         <AppContext.Provider value={{ mode, setMode }}>
