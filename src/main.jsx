167d13ddc62f9d284ad616617a3326e8e0405844
myokoaung002 <myokoaung002@gmail.com>
2024-11-28 18:12:14 +0630

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,15 @@
-/*
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./index.css";
 import ThemedApp from './ThemedApp.jsx'
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemedApp />
   </StrictMode>,
-);
-*/
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import ThemedApp from "./ThemedApp";
-ReactDOM.createRoot(document.getElementById("root")).render(
- <React.StrictMode>
- <ThemedApp />
- </React.StrictMode>
 );
