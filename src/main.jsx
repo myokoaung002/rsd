f36d5e848d92d69c812a7a5a3f267f01ba594c90
myokoaung002 <myokoaung002@gmail.com>
2024-11-28 21:55:44 +0630

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "./index.css";
 import ThemedApp from './ThemedApp.jsx'
 
 import '@fontsource/roboto/300.css';
