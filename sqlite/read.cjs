b8257b512c29a4703fa8b5ae1a0d4babea901a28
myokoaung002 <myokoaung002@gmail.com>
2024-11-30 19:08:50 +0630

    commit

---
+++ sqlite/read.cjs
@@ -0,0 +1,14 @@
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+prisma.user.findFirst({
+    where: { id: 1 },
+    include: { posts: true },
+}).then( data => console.log(data))
+.catch(e => {
+    console.error(e);
+    ProcessingInstruction.exit(1);
+}).finally(() => {
+    prisma.$disconnect();
+});
